3e878b4145f9b768e30e36c9e1adbcb2d0c4bfe5
etienne <etienne.desautels@gmail.com>
2012-11-05 14:26:43 -0500

    Fixed settings to support Django 1.3 and 1.4 (problem with ADMIN_MEDIA_PREFIX).

--- filebrowser/settings.py
+++ filebrowser/settings.py
@@ -13,7 +13,10 @@ try:
     DEFAULT_URL_TINYMCE = tinymce.settings.JS_BASE_URL + '/'
     DEFAULT_PATH_TINYMCE = tinymce.settings.JS_ROOT + '/'
 except ImportError:
-    DEFAULT_URL_TINYMCE = settings.ADMIN_MEDIA_PREFIX + "tinymce/jscripts/tiny_mce/"
+    if hasattr(settings, 'ADMIN_MEDIA_PREFIX'):
+        DEFAULT_URL_TINYMCE = settings.ADMIN_MEDIA_PREFIX + "tinymce/jscripts/tiny_mce/"
+    else:
+        DEFAULT_URL_TINYMCE = settings.STATIC_URL + "admin/tinymce/jscripts/tiny_mce/"
     DEFAULT_PATH_TINYMCE = os.path.join(settings.MEDIA_ROOT, 'admin/tinymce/jscripts/tiny_mce/')
 
 # Set to True in order to see the FileObject when Browsing.
